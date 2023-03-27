8f2848a6feff425c52748c6f88589da833debf5c
Ayan Ahmed <ayanahmed@ayans-mbp.home>
2023-03-27 20:41:34 +0100

    set up the sidebar as a template

---
+++ client/src/components/Sidebar.tsx
@@ -0,0 +1,34 @@
+import React from "react";
+
+function Sidebar() {
+	return (
+		<div>
+			return (
+			<div className="h-screen bg-gray-200">
+				<div className="p-6">
+					<h2 className="text-lg font-medium">Dashboard</h2>
+					<ul className="mt-4">
+						<li className="mb-4">
+							<a href="#" className="text-gray-600 hover:text-gray-800">
+								Home
+							</a>
+						</li>
+						<li className="mb-4">
+							<a href="#" className="text-gray-600 hover:text-gray-800">
+								About
+							</a>
+						</li>
+						<li className="mb-4">
+							<a href="#" className="text-gray-600 hover:text-gray-800">
+								Contact
+							</a>
+						</li>
+					</ul>
+				</div>
+			</div>
+			);
+		</div>
+	);
+}
+
+export default Sidebar;
